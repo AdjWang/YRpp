bdad83f40692fb404c994c42c269227a1d98fb28
AdjWang <wwang230513@gmail.com>
2026-01-12 18:03:05 +0800

    add MapCell

--- Syringe.h
+++ Syringe.h
@@ -222,7 +222,7 @@ public:
 //Use this for DLL export functions
 //e.g. EXPORT FunctionName(REGISTERS* R)
 #define EXPORT extern "C" __declspec(dllexport) DWORD __cdecl
-#define EXPORT_FUNC(name) extern "C" __declspec(dllexport) DWORD __cdecl name (REGISTERS *R)
+#define EXPORT_FUNC(name) extern "C" __declspec(dllexport) DWORD __cdecl name (yrpp::REGISTERS *R)
 
 
 //Handshake definitions
